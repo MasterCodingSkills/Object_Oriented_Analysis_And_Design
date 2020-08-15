7dd088f92594b2f0f10222c3fc626cc97ec3ea4e
MasterCodingSkills <56904368+MasterCodingSkills@users.noreply.github.com>
2020-08-15 19:29:34 +0530

    Added first test case and changes in FindGuitarTester class to make it accessible for unit testing.

---
+++ src/test/java/com/manish/guittershop/GuitterSeachTesting.java
@@ -0,0 +1,21 @@
+package com.manish.guittershop;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class GuitterSeachTesting {
+	
+	@Test
+	public void testSearchGuitter() {
+		 String expected = "Erin, you might like this fender Stratocaster electric guitar:\n   " + 
+		 		"Alderback and sides, \n   " + 
+		 		"Alder top.\n" + 
+		 		"You can have it for only $5.0!";
+		 
+		 Inventory inventory = new Inventory();
+		 FindGuitarTester.initializeInventory(inventory);
+		 
+	     assertEquals(expected,FindGuitarTester.searchGuitter(inventory));
+	}
+}
